329f0231719d5d0215822b58c8b490305e842d49
PONNIMOL <nimolsetec@gmail.com>
2014-11-20 09:01:37 +0700

    update Exception comment

--- src/view/SettleOrder.java
+++ src/view/SettleOrder.java
@@ -109,11 +109,10 @@ public class SettleOrder extends MasterFrame {
 	
 	public static void main(String[] args) {
 		try {
-//			UIManager.put("Synthetica.window.decoration", Boolean.FALSE);
-//			UIManager.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaAluOxideLookAndFeel");
-			UIManager.setLookAndFeel("com.jtattoo.plaf.graphite.GraphiteLookAndFeel");			
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e) {
+			UIManager.put("Synthetica.window.decoration", Boolean.FALSE);
+			UIManager.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaAluOxideLookAndFeel");
+//			UIManager.setLookAndFeel("com.jtattoo.plaf.graphite.GraphiteLookAndFeel");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		EventQueue.invokeLater(new Runnable() {			
